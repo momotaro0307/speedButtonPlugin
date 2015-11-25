fe4c5487de815d48c15e53d531dc4ac5845556e3
Nishimura Tomoyuki <t.nishimura.tomoyuki@gmail.com>
2015-11-25 11:25:43 +0900

    Add uglify plugin in order to generate the min file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,17 @@ module.exports = function(grunt) {
     'use strict';
  
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            default: {
+                src: './js/UlizaPlayerJS.v1.8.speedButtonPlugin.js',
+                dest: './dist/<%= pkg.name %>.min.js'
+            }
+        }
     });
 
     require('load-grunt-tasks')(grunt);
 
-    grunt.registerTask('default',[]);
+    grunt.registerTask('default',['uglify']);
  
 };
